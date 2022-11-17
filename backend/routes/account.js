c369cb9709156f6cb01babc0b4943eadef9466b2
wesoa012 <wesa@smu.edu>
2022-11-17 10:48:26 -0600

    able to get all accounts and change role_id of a singular one

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -9,24 +9,42 @@ router.use(bodyParser.json());
 router.get('/:id', async (req, res, next) => {
     let accountId = parseInt(req.params.id);
     //let accountId = req.query.id; //Don't know whether params or body will be used
-    if(typeof(accountId) !== 'number' || !accountId)
-    {
+    if (typeof (accountId) !== 'number' || !accountId) {
         console.log("account_id is not type Number");
         res.status(400).send();
     }
-    else{
-        const tenantByID = await req.models.tenant.fetchTenantByID(accountId);
+    else {
+        const accountByID = await req.models.account.fetchAccountBySmu_id(accountId);
         //will return 404 not found if id does not exist
-        if(JSON.stringify(tenantByID) == '[]')
-        {
+        if (JSON.stringify(accountByID) == '[]') {
             console.log("No user found with id", accountId)
             res.status(404).send();
         }
-        else
-        {
-            res.json(tenantByID);
+        else {
+            res.json(accountByID);
         }
     }
+    next();
+});
+
+router.put('/:id/role', async (req, res, next) => {
+    let accountId = parseInt(req.params.id);
+    //let accountId = req.query.id; //Don't know whether params or body will be used
+    if (typeof (accountId) !== 'number' || !accountId) {
+        console.log("account_id is not type Number");
+        res.status(400).send();
+    }
+    else {
+        const accountByID = await req.models.account.changeRole(accountId, res.new_role);
+        res.status = 200;
+        res.json(accountByID);
+    }
+    next();
+});
+
+router.get("/", async (req, res, next) => {
+    const accounts = await req.models.account.fetchAllAccounts();
+    res.json(accounts)
     next();
 });
 
