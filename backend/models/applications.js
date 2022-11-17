4db49acaa330d8e1060783d3848fabce445ed8df
Peyton984 <pmaass@smu.edu>
2022-11-17 11:10:00 -0600

    route fixes

--- backend/models/applications.js
+++ backend/models/applications.js
@@ -1,5 +1,5 @@
 const knex = require('../database/knex');
-const ACCOUNT_TABLE = 'applications';
+const APPLICATIONS_TABLE = 'applications';
 
 const createApplication = async (body) => {
     const application_num = body.application_num;
@@ -10,25 +10,25 @@ const createApplication = async (body) => {
 }
 
 const getAllApplications = async () => {
-    const query = knex(APPLICATION_TABLE)
+    const query = knex(APPLICATIONS_TABLE)
     const results = await query;
     return results;
 }
 
 const getByApplicationNum = async (application_num) => {
-    const query = knex(APPLICATION_TABLE).where({ application_num })
+    const query = knex(APPLICATIONS_TABLE).where({ application_num })
     const results = await query;
     return results;
 }
 
 const getBySMUId = async (smu_id) => {
-    const query = knex(APPLICATION_TABLE).where({ smu_id })
+    const query = knex(APPLICATIONS_TABLE).where({ smu_id })
     const results = await query;
     return results;
 }
 
 const getByGrade = async (grade) => {
-    const query = knex(APPLICATION_TABLE).where({ grade })
+    const query = knex(APPLICATIONS_TABLE).where({ grade })
     const results = await query;
     return results;
 }
--- backend/models/course.js
+++ backend/models/course.js
@@ -1,5 +1,5 @@
 const knex = require('../database/knex');
-const ACCOUNT_TABLE = 'courses';
+const COURSES_TABLE = 'courses';
     
 const createCourse = async (body) => {
     const course_id = body.course_id;
@@ -11,25 +11,25 @@ const createCourse = async (body) => {
 }
 
 const getAllCourses = async () => {
-    const query = knex(COURSE_TABLE);
+    const query = knex(COURSES_TABLE);
     const results = await query;
     return results;
 }
 
 const getByCourseId = async (course_id) => {
-    const query = knex(COURSE_TABLE).where({ course_id })
+    const query = knex(COURSES_TABLE).where({ course_id })
     const results = await query;
     return results;
 }
 
 const getByCourseName = async (course_name) => {
-    const query = knex(COURSE_TABLE).where({ course_name })
+    const query = knex(COURSES_TABLE).where({ course_name })
     const results = await query;
     return results;
 }
 
 const getByProfessorId = async (course_name) => {
-    const query = knex(COURSE_TABLE).where({ professor_id })
+    const query = knex(COURSES_TABLE).where({ professor_id })
     const results = await query;
     return results;
 }
@@ -47,20 +47,20 @@ const updateCourseData = async (course_id, professor_id) => {
     if(syllabus !== undefined) {
         this.updateSyllabus(course_id, professor_id);
     }
-    const newRecord = knex(COURSE_TABLE).where({ course_id });
+    const newRecord = knex(COURSES_TABLE).where({ course_id });
     return newRecord;
 }
 
 const updateCourseName = async (course_id, professor_id) => {
-    const course_name = knex(COURSE_TABLE).where({ course_id }).where({ professor_id }).set({ course_name });
+    const course_name = knex(COURSES_TABLE).where({ course_id }).where({ professor_id }).set({ course_name });
 }
 
 const updateDescription = async (course_id, professor_id) => {
-    const description = knex(COURSE_TABLE).where({ course_id }).where({ professor_id }).set({ description });
+    const description = knex(COURSES_TABLE).where({ course_id }).where({ professor_id }).set({ description });
 }
 
 const updateSyllabus = async (course_id, professor_id) => {
-    const syllabus = knex(COURSE_TABLE).where({ course_id }).where({ professor_id }).set({ syllabus });
+    const syllabus = knex(COURSES_TABLE).where({ course_id }).where({ professor_id }).set({ syllabus });
 }
 
 module.exports = {
