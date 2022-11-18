d983f1a5a2d3db07d00fd6f4dad37cd31f5385fa
wesoa012 <wesa@smu.edu>
2022-11-18 01:16:40 -0600

    Nothing for now, May change course view to have this object in future

---
+++ frontend/src/Components/Applications/CreateApplication.jsx
@@ -0,0 +1,30 @@
+// import FormControl from '@mui/material/FormControl';
+// import IconButton from '@mui/material/IconButton';
+// import OutlinedInput from '@mui/material/OutlinedInput';
+// import InputLabel from '@mui/material/InputLabel';
+// import InputAdornment from '@mui/material/InputAdornment';
+// import Checkbox from '@mui/material/Checkbox';
+
+
+// //May come back to this, but for now it is is course view
+
+// <FormControl sx={{ m: 0, width: '100%' }} variant="outlined" className="">
+// <InputLabel htmlFor="course-grade">Course Grade</InputLabel>
+// <OutlinedInput
+//     id="course-grade"
+//     type='number'
+//     value={grade}
+//     onChange={event => setGrade(event.target.value)}
+//     label="Course Grade"
+// />
+// </FormControl>
+// {timeslots.map((timeslot) => {
+//     return (
+//         <div key={timeslot.id}>
+//             <Checkbox
+//                 checked={applicationTimes.indexOf(timeslot) !== -1}
+//                 onChange={() => checkTimeslot(timeslot)}
+//                 inputProps={{ 'aria-label': 'controlled' }}
+//             />
+//         </div>)
+// })}
