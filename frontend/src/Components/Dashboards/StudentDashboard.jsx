b08ac4a0a095fcd6c4259bcbda7488510698d636
wesoa012 <wesa@smu.edu>
2022-11-18 09:59:18 -0600

    Fixed routing issue for students

--- frontend/src/Components/Dashboards/StudentDashboard.jsx
+++ frontend/src/Components/Dashboards/StudentDashboard.jsx
@@ -12,7 +12,7 @@ export const StudentDashboard = () => {
 
     return (<div className="Dashboard text-center">
         
-            <Button variant="contained" className="btn btn-primary" onClick={() => {navigate("/applications")}}>View Courses</Button>
+            <Button variant="contained" className="btn btn-primary" onClick={() => {navigate("/courses")}}>View Courses</Button>
             <br/>
             <br/>
             <Button variant="contained" className="btn btn-primary" onClick={() => {navigate(`/accounts/${currUser.smu_id}/applications`)}}>Submitted Applications</Button>
